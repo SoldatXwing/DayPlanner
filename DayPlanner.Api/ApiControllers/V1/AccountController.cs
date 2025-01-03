ce38bf15239e37b7ec39720ee5021a38190227ab
Suiram1701 <110390261+Suiram1701@users.noreply.github.com>
2025-01-03 22:43:56 +0100

    Added api documentation and improved some endpoints

--- DayPlanner.Api/ApiControllers/V1/AccountController.cs
+++ DayPlanner.Api/ApiControllers/V1/AccountController.cs
@@ -3,6 +3,7 @@ using DayPlanner.Abstractions.Models.Backend;
 using DayPlanner.Abstractions.Models.DTO;
 using DayPlanner.Abstractions.Services;
 using DayPlanner.Api.Extensions;
+using DayPlanner.Authorization.Exceptions;
 using FirebaseAdmin.Auth;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -10,12 +11,22 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DayPlanner.Api.ApiControllers.V1;
 
+/// <summary>
+/// Manages user login and registration.
+/// </summary>
 [ApiController]
 [ApiVersion(1)]
 [Route("v{version:apiVersion}/account")]
 public sealed class AccountController : ControllerBase
 {
+    /// <summary>
+    /// Gets the login provider data associated with the user of the current session.
+    /// </summary>
+    /// <response code="200">Success - Returns a collection of provider data associated with the user</response>
+    /// <response code="404">Not found - The user were internally not found</response>
     [HttpGet]
+    [ProducesResponseType<IUserInfo[]>(200)]
+    [ProducesResponseType<ApiErrorModel>(404)]
     public async Task<IActionResult> GetAccountInformationAsync([FromServices] IUserService userService)
     {
         string userId = HttpContext.User.GetUserId()!;
@@ -26,21 +37,36 @@ public sealed class AccountController : ControllerBase
         return Ok(user.ProviderData);
     }
 
+    /// <summary>
+    /// Tries to sign in for a user.
+    /// </summary>
+    /// <param name="request">Provided data for the sign in.</param>
+    /// <param name="jwtProvider"></param>
+    /// <response code="200">Success - Returns the complete bearer access token</response>
+    /// <response code="400">Bad request - Invalid email or password</response>
     [AllowAnonymous]
     [HttpPost("login")]
+    [ProducesResponseType<ApiErrorModel>(400)]
     public async Task<IActionResult> LoginAsync([FromBody] UserRequest request, [FromServices] IJwtProvider jwtProvider)
     {
         try
         {
-            var token = await jwtProvider.GetForCredentialsAsync(request.Email, request.Password);
-            return Ok(new { Token = token });
+            string token = await jwtProvider.GetForCredentialsAsync(request.Email, request.Password);
+            return Ok(token);
         }
-        catch (Exception ex)
+        catch (Exception ex) when (ex.GetType() == typeof(BadCredentialsException)|| ex.GetType() == typeof(InvalidEmailException))
         {
-            return BadRequest(new ApiErrorModel { Error = ex.Message, Message = "User not found" });
+            return BadRequest(new ApiErrorModel { Error = ex.Message, Message = "Invalid email or password." });
         }
     }
 
+    /// <summary>
+    /// Validates the current login token.
+    /// </summary>
+    /// <param name="authorization">The login token to verify.</param>
+    /// <param name="authService"></param>
+    /// <response code="200">Success - Returns the Uid of the signed in user</response>
+    /// <response code="401">Unauthorized - The specified token is invalid and cannot be used</response>
     [AllowAnonymous]
     [HttpPost("validate")]
     public async Task<IActionResult> ValidateTokenAsync([FromHeader] string authorization, [FromServices] IAuthService authService)
@@ -52,7 +78,7 @@ public sealed class AccountController : ControllerBase
         try
         {
             FirebaseToken firebaseToken = await authService.VerifyTokenAsync(token);
-            return Ok(new { firebaseToken.Uid });
+            return Ok(firebaseToken.Uid);
         }
         catch
         {
@@ -60,16 +86,24 @@ public sealed class AccountController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Registers a new user.
+    /// </summary>
+    /// <param name="request">Provided data for the new user.</param>
+    /// <param name="userService"></param>
+    /// <response code="200">Success - Returns the Uid of the created user</response>
+    /// <response code="400">Bad request - Invalid data were provided</response>
     [AllowAnonymous]
     [HttpPost("register")]
+    [ProducesResponseType<ApiErrorModel>(400)]
     public async Task<IActionResult> RegisterUserAsync([FromBody] RegisterUserRequest request, [FromServices] IUserService userService)
     {
         if (string.IsNullOrEmpty(request.Email) || string.IsNullOrEmpty(request.Password))
-            return BadRequest("Email and password are required.");
+            return BadRequest(new ApiErrorModel { Error = "Invalid data", Message = "Email and password are required." });
 
         try
         {
-            var userRecordArgs = new UserRecordArgs
+            UserRecordArgs userRecordArgs = new()
             {
                 Email = request.Email,
                 Password = request.Password,
@@ -77,7 +111,7 @@ public sealed class AccountController : ControllerBase
             };
 
             UserRecord userRecord = await userService.CreateUserAsync(userRecordArgs);
-            return Ok(new { Message = "User registered successfully.", userRecord.Uid });
+            return Ok(userRecord.Uid);
         }
         catch (Exception ex)
         {
--- DayPlanner.Api/ApiControllers/V1/AppointmentController.cs
+++ DayPlanner.Api/ApiControllers/V1/AppointmentController.cs
@@ -3,19 +3,26 @@ using DayPlanner.Abstractions.Models.Backend;
 using DayPlanner.Abstractions.Models.DTO;
 using DayPlanner.Abstractions.Services;
 using DayPlanner.Api.Extensions;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Net.Http;
-using static Google.Rpc.Context.AttributeContext.Types;
 
 namespace DayPlanner.Api.ApiControllers.V1;
 
+/// <summary>
+/// Manages appointments of a user.
+/// </summary>
 [ApiController]
 [ApiVersion(1)]
-[Route("v{version:apiVersion}/appointment")]
+[Route("v{version:apiVersion}/appointments")]
 public sealed class AppointmentController(IAppointmentsService appointmentService) : ControllerBase
 {
+    /// <summary>
+    /// Get all appointments of the signed in user.
+    /// </summary>
+    /// <param name="page">The item page to return.</param>
+    /// <param name="pageSize">The amount of items to return per page.</param>
+    /// <response code="200">Success - Returns a collection with the appointments</response>
     [HttpGet]
+    [ProducesResponseType<PaginatedResponse<Appointment>>(200)]
     public async Task<IActionResult> GetAllAppointmentsAsync([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
     {
         string userId = HttpContext.User.GetUserId()!;
@@ -32,23 +39,39 @@ public sealed class AppointmentController(IAppointmentsService appointmentServic
         }
     }
 
+    /// <summary>
+    /// Get all appointments of the signed in user that are inside a start and end date.
+    /// </summary>
+    /// <remarks>
+    /// The <paramref name="end"/> have to be larger than <paramref name="start"/>.
+    /// </remarks>
+    /// <param name="start">The start date.</param>
+    /// <param name="end">The end date.</param>
+    /// <response code="200">Success - Returns a collection with the appointments</response>
+    /// <response code="401"></response>
     [HttpGet("range")]
+    [ProducesResponseType<Appointment[]>(200)]
     public async Task<IActionResult> GetAppointmentsByDateAsync([FromQuery] DateTime start, [FromQuery] DateTime end)
     {
         if (start > end)
             return BadRequest(new ApiErrorModel { Message = "Invalid date times", Error = "Start date cant be greater than end date." });
         try
         {
-            var appointments = await appointmentService.GetUsersAppointments(HttpContext.User.GetUserId()!, start, end);
-            return Ok(appointments);
+            return Ok(await appointmentService.GetUsersAppointments(HttpContext.User.GetUserId()!, start, end));
         }
         catch (UnauthorizedAccessException)
         {
-            return Unauthorized();
+            return Forbid();
         }
     }
 
+    /// <summary>
+    /// Creates a new appointment for the signed in user.
+    /// </summary>
+    /// <param name="request">The appointment to create.</param>
+    /// <response code="201">Success - Returns the created appointment</response>
     [HttpPost]
+    [ProducesResponseType<Appointment>(201)]
     public async Task<IActionResult> CreateAppointmentAsync([FromBody] AppointmentRequest request)
     {
         if (string.IsNullOrEmpty(request.Title) ||
@@ -59,51 +82,77 @@ public sealed class AppointmentController(IAppointmentsService appointmentServic
         }
 
         Appointment appointment = await appointmentService.CreateAppointment(HttpContext.User.GetUserId()!, request);
-        return CreatedAtAction(
-            actionName: nameof(GetAppointmentAsync),
-            routeValues: new { appointmentId = appointment.Id },
-            value: appointment);
+        return Created($"/v1/appointments/{appointment.Id}", appointment);
     }
 
+    /// <summary>
+    /// Retrieves an appointment with a certain id.
+    /// </summary>
+    /// <param name="appointmentId">The id of the appointment to get.</param>
+    /// <response code="200">Success - Returns the appointment with the id</response>
+    /// <response code="404">Not found - No appointment with the id was found</response>
     [HttpGet("{appointmentId}")]
+    [ProducesResponseType<Appointment>(200)]
+    [ProducesResponseType<ApiErrorModel>(404)]
     public async Task<IActionResult> GetAppointmentAsync([FromRoute] string appointmentId)
     {
         Appointment? appointment = await appointmentService.GetAppointmentById(HttpContext.User.GetUserId()!, appointmentId);
         return appointment is null
-            ? NotFound(new ApiErrorModel { Message = "Appointment not found", Error = $"No appointment with id {appointmentId} found." }) 
+            ? AppointmentNotFound(appointmentId)
             : Ok(appointment);
     }
 
+    /// <summary>
+    /// Updates an appointment of a user.
+    /// </summary>
+    /// <remarks>
+    /// The whole appointment except of the id will be updated so values that shouldn't be changed must be set to their current value.
+    /// </remarks>
+    /// <param name="appointmentId">The id of the appointment to update.</param>
+    /// <param name="request">The updated appointment.</param>
+    /// <response code="200">Success - Returns the updated appointment</response>
+    /// <response code="403">Forbidden - The signed in user doesn't have access to this appointment</response>
     [HttpPut("{appointmentId}")]
+    [ProducesResponseType<Appointment>(200)]
     public async Task<IActionResult> UpdateAppointmentAsync([FromRoute] string appointmentId, [FromBody] AppointmentRequest request)
     {
         try
         {
-            var appointment = await appointmentService.UpdateAppointment(appointmentId, HttpContext.User.GetUserId()!, request);
+            Appointment appointment = await appointmentService.UpdateAppointment(appointmentId, HttpContext.User.GetUserId()!, request);
             return Ok(appointment);
         }
         catch (UnauthorizedAccessException)
         {
-            return Unauthorized();
+            return Forbid();
         }
     }
 
+    /// <summary>
+    /// Removes an appointment of a user.
+    /// </summary>
+    /// <param name="appointmentId">The if of the appointment to remove.</param>
+    /// <response code="204">Success - The appointment were successfully removed</response>
+    /// <response code="403">Forbidden - The signed in user doesn't have access to this appointment</response>
+    /// <response code="404">Not found - No appointment with the id was found</response>
     [HttpDelete("{appointmentId}")]
+    [ProducesResponseType<ApiErrorModel>(404)]
     public async Task<IActionResult> DeleteAppointmentAsync([FromRoute] string appointmentId)
     {
         try
         {
             await appointmentService.DeleteUsersAppointment(HttpContext.User.GetUserId()!, appointmentId);
-            return Ok();
+            return NoContent();
         }
         catch (UnauthorizedAccessException)
         {
             //TODO: log
-            return Unauthorized();
+            return Forbid();
         }
         catch (InvalidOperationException ex)
         {
             return NotFound(new ApiErrorModel { Message = "Appointment not found", Error = ex.Message });
         }
     }
+
+    private NotFoundObjectResult AppointmentNotFound(string id) => NotFound(new ApiErrorModel { Message = "Appointment not found", Error = $"No appointment with id {id} found." });
 }
