317ababd63d2f88fa58cb59bb65999252f44d970
SoldatXwing <soldatxwing@gmail.com>
2025-01-03 18:54:28 +0100

    Create project and add service for external appointment data import

---
+++ DayPlanner.Abstractions/Services/IExternalAppointmentService.cs
@@ -0,0 +1,10 @@
+﻿using DayPlanner.Abstractions.Models.Backend;
+
+namespace DayPlanner.Abstractions.Services
+{
+    public interface IExternalAppointmentService
+    {
+        Task<List<Appointment>> GetAppointments(DateTime start, DateTime end);
+    }
+
+}
