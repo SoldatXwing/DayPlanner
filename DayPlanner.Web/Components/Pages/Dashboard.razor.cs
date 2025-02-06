94f5ca81af7fb3ba4c7fc15a29bfbe5d6025020b
SoldatXwing <soldatxwing@gmail.com>
2025-02-06 21:41:48 +0100

    Fixed Appointment render bug in Dashboard Fixes #3

--- DayPlanner.Web/Components/Pages/Dashboard.razor.cs
+++ DayPlanner.Web/Components/Pages/Dashboard.razor.cs
@@ -25,34 +25,32 @@ public partial class Dashboard : ComponentBase
         {
             _appointments = await AppointmentService.GetAllAppointmentsInRangeAsync(args.Start, args.End);
             _loadedRange = (args.Start, args.End);
+            StateHasChanged();
         }
         else
         {
             var (loadedFrom, loadedTo) = _loadedRange.Value;
 
-            // Falls der neue Bereich außerhalb des "linken" kalendarblatts liegt
+            //User navigated in the schedular to the "left"
             if (args.Start < loadedFrom)
             {
                 var newAppointmentsLeft = await AppointmentService
                     .GetAllAppointmentsInRangeAsync(args.Start, loadedFrom);
 
-                // Zusammenführen der Listen, ggf. Duplikate entfernen
-                _appointments.AddRange(newAppointmentsLeft);
+                _appointments = [.. _appointments, .. newAppointmentsLeft];
             }
-
-            // Falls der neue Bereich außerhalb des "rechten" kalendarblatts liegt
+            //User navigated in the schedular to the "right"
             if (args.End > loadedTo)
             {
                 var newAppointmentsRight = await AppointmentService
                     .GetAllAppointmentsInRangeAsync(loadedTo, args.End);
 
-                _appointments.AddRange(newAppointmentsRight);
+                _appointments = [.. _appointments, .. newAppointmentsRight]; // its necessary to create a "new" list, to notfiy that the list changed.
             }
 
-            // Gemerkten Bereich auf den neuen ausdehnen
-            var newStart = args.Start < loadedFrom ? args.Start : loadedFrom;
-            var newEnd = args.End > loadedTo ? args.End : loadedTo;
-            _loadedRange = (newStart, newEnd);
+            _loadedRange = (new DateTime(Math.Min(args.Start.Ticks, loadedFrom.Ticks)),
+                            new DateTime(Math.Max(args.End.Ticks, loadedTo.Ticks)));
+            StateHasChanged();
         }
     }
     private void OnAppointmentMouseLeave(SchedulerAppointmentMouseEventArgs<Appointment> args)
