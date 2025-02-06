6e4b9c5cb11080b3f9b623bdd5ba1396543b76b0
Suiram1701 <110390261+Suiram1701@users.noreply.github.com>
2025-02-06 15:10:41 +0100

    Moved most inline styles code into CSS isolation -Removed Bootstrap files -Some other minor improvements

--- DayPlanner.Web/Components/Pages/Dashboard.razor.cs
+++ DayPlanner.Web/Components/Pages/Dashboard.razor.cs
@@ -1,44 +1,26 @@
 ﻿using DayPlanner.Abstractions.Models.Backend;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Localization;
-using System.Globalization;
+using Radzen.Blazor;
 
 namespace DayPlanner.Web.Components.Pages;
 
-[Route("/dashboard")]
 [Authorize]
+[Route("/dashboard")]
 public partial class Dashboard : ComponentBase
 {
     [Inject]
-    private IHttpContextAccessor HttpContextAccessor { get; set; } = default!;
-    [Inject]
     private IStringLocalizer<Dashboard> Localizer { get; set; } = default!;
-    private CultureInfo CultureInfo { get; set; } = default!;
 
-    private List<Appointment> _appointments = [];
-    protected async override Task OnInitializedAsync()
+    private List<Appointment> _appointments = [
+        new Appointment{UserId = "s", Title = "Meeting with John Doe", Start = DateTime.Now, End = DateTime.Now.AddHours(1) },
+        new Appointment{UserId = "s", Title = "Meeting with Brohn Doe", Start = DateTime.Now, End = DateTime.Now.AddHours(2) },
+        new Appointment{UserId = "s", Title = "Meeting with Klone Doe", Start = DateTime.Now, End = DateTime.Now.AddHours(3) },
+    ];
+
+    private void OnAppointmentMouseLeave(SchedulerAppointmentMouseEventArgs<Appointment> args)
     {
-        IRequestCultureFeature cultureFeature = HttpContextAccessor.HttpContext!.Features.GetRequiredFeature<IRequestCultureFeature>();
-        CultureInfo = cultureFeature.RequestCulture.UICulture;
-        _appointments =
-        [
-            new Appointment{UserId = "s", Title = "Meeting with John Doe", Start = DateTime.Now, End = DateTime.Now.AddHours(1) },
-            new Appointment{UserId = "s", Title = "Meeting with Brohn Doe", Start = DateTime.Now, End = DateTime.Now.AddHours(2) },
-            new Appointment{UserId = "s", Title = "Meeting with Klone Doe", Start = DateTime.Now, End = DateTime.Now.AddHours(3) },
-        ];
+        TooltipService.Close();
     }
-    protected override void OnAfterRender(bool firstRender)
-    {
-        //Keep the red current time line active
-        if (firstRender)
-        {
-            var timer = new Timer(_ => InvokeAsync(StateHasChanged));
-            timer.Change(TimeSpan.FromSeconds(30), TimeSpan.FromSeconds(30));
-        }
-    }
-
-
 }
